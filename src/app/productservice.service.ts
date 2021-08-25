987b0dfae752fb181c67ba4d29a37535e31508d4
Sonali <sonali.umare981@gmail.com>
2021-08-26 01:47:24 +0530

    environment details changed

--- src/app/productservice.service.ts
+++ src/app/productservice.service.ts
@@ -2,24 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Product } from './Product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductserviceService {
-
+  baseURL=environment.baseURL;
   constructor(private _http: HttpClient) { }
 
  fetchAllProductList():Observable<Product[]>{
-   return this._http.get<Product[]>("http://localhost:8080/getproductlist");
+   return this._http.get<Product[]>(this.baseURL+"/getproductlist");
  } 
  addProduct(product:Product):Observable<Product>{
-  return this._http.post<Product>("http://localhost:8080/addproduct",product);
+  return this._http.post<Product>(this.baseURL+"/addproduct",product);
 } 
 fetchProduct(id:number):Observable<Product>{
-  return this._http.get<Product>("http://localhost:8080/getproduct/"+id);
+  return this._http.get<Product>(this.baseURL+"/getproduct/"+id);
 } 
 deleteProduct(id:number):Observable<Product>{
-  return this._http.delete<Product>("http://localhost:8080/deleteproduct/"+id);
+  return this._http.delete<Product>(this.baseURL+"/deleteproduct/"+id);
 }
 }
